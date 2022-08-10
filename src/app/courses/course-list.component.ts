63860da609334d2cbb7fca9d13fc7aa9394f6689
Rejane Guedes <rejane.cristian@gmail.com>
2022-08-09 21:37:16 -0300

    Fazendo requisições em HTTP

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -22,15 +22,25 @@ export class CourseListComponent implements OnInit{
   }
 
   ngOnInit(): void{
-      this._courses = this.courseService.retrieveAll();
-      this._filteredCourses = this._courses;
+      this.retrieveAll();
+
+  }
+
+  retrieveAll(): void{
+      this.courseService.retrieveAll(). subscribe({
+          next: courses=>{
+            this._courses = courses;
+            this._filteredCourses = this._courses;
+          },
+          error: err => console.log('Error',err)
+      })
 
   }
 
   set filter(value: string){
       this._filterBy = value;
 
-      this._filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleUpperCase().indexOf(this._filterBy.toLocaleUpperCase()) >-1);
+      this._filteredCourses = this._courses.filter((course: Course) => course.name!.toLocaleUpperCase().indexOf(this._filterBy.toLocaleUpperCase()) >-1);
 
   }
   get filter(){
--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -1,5 +1,7 @@
 import { Course } from './course';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -7,17 +9,23 @@ import { Injectable } from '@angular/core';
 
 export class CourseService{
 
-  retrieveAll(): Course[]{
-    return COURSES;
+  private coursesUrl: string = 'http://localhost:3100/api/courses';
+
+  constructor(private httpClient:HttpClient){}
+
+  retrieveAll(): Observable<Course[]>{
+    return this.httpClient.get<Course[]>(this.coursesUrl);
   }
 
-  retrieveById(id: number): Course {
+  retrieveById(id: number): Observable<Course> {
 
-    return COURSES[id];
+    return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
 }
-save(course: Course): void {
+save(course: Course): Observable<Course> {
   if(course.id){
-    COURSES[course.id] = course
+    return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course);
+  } else{
+    return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
   }
 
 }
